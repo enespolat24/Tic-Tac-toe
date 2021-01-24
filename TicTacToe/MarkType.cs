9c04c57df67c96f0f5867f9ae7d42218cf0b1c20
enes polat <enespolat24@gmail.com>
2021-01-24 16:52:04 +0300

    fresh cone //contains an enum

--- TicTacToe/MarkType.cs
+++ TicTacToe/MarkType.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class MarkType
+    /// <summary>
+    /// The type of value a cell in the gameboard is currently at
+    /// </summary>
+   public enum MarkType
     {
-        Console.Writeline("hello");
+        Free,//the cell hasn't been clicked yet
+        Nought,// the cell is an O
+        Cross//the cell is an X
     }
 }
