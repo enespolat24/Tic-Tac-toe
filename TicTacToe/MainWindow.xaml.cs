08069a605680c2d4cb41b5e7792ef2b5be142ae0
enes polat <enespolat24@gmail.com>
2021-01-24 18:02:55 +0300

    update

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -21,7 +21,12 @@ namespace TicTacToe
     public partial class MainWindow : Window
     {
         #region Private members
-
+        //holds the current results of cells in the active game
+        private MarkType[] mResults;
+        //true if it is player1's turn (x) or player 2's turn (0)
+        private bool mPlayer1Turn;
+        //tru if the game has ended
+        private bool mGameEnded;
 
 
         #endregion
@@ -38,10 +43,41 @@ namespace TicTacToe
 
         }
         #endregion
+
+
+        //Starts a new game and clear the grid
         private void NewGame()
         {
-            throw new NotImplementedException();
+            //create new blank array of empty cell
+            mResults = new MarkType[9];
+            for (int i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] = MarkType.Free;
+            }
+            //make sure player 1 starts the game
+            mPlayer1Turn = true;
+
+            //interate every button on the grid
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+                button.Background = Brushes.PapayaWhip;
+                button.Foreground = Brushes.Blue;
+            });
+            mGameEnded = false; //the game has not finished yet
         }
 
+        //button click event
+        //sender => the button
+        //e => the events of the click
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //starts a new game on the click after it finished
+            if (mGameEnded)
+            {
+                NewGame();
+                return;
+            }
+        }
     }
 }
